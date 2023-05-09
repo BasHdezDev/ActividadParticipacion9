61cdc227a71c089eb70e2ddacf36e38e1a59e7ae
Bas <bysebashdez@gmail.com>
2023-05-09 11:44:11 -0500

    Second try of print the data, i tried readlines function

--- DatosMeteorologicos/datos.py
+++ DatosMeteorologicos/datos.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Tuple
 
 
 class DatosMeteorologicos:
@@ -13,45 +13,40 @@ class DatosMeteorologicos:
             presion_total = 0
             velocidad_total_viento = 0
 
-            for linea in f:
+            data = f.readlines()
+
+            for linea in data:
                 if linea.startswith("Estación"):
                     estacion = linea.split(':')
-                    print(estacion)
 
                 elif linea.startswith("Latitud"):
                     latitud = linea.split(':')
-                    print(latitud)
 
                 elif linea.startswith("Longitud"):
                     longitud = linea.split(':')
-                    print(longitud)
 
                 elif linea.startswith("Fecha"):
                     fecha = linea.split(':')
-                    print(fecha)
 
                 elif linea.startswith("Hora"):
                     hora = linea.split(':')
-                    print(hora)
 
                 elif linea.startswith("Temperatura"):
                     temperatura = linea.split(':')
                     temperatura_total += temperatura
-                    print(temperatura_total)
 
                 elif linea.startswith("Humedad"):
                     humedad = linea.split(':')
                     humedad_total += humedad
-                    print(humedad_total)
 
                 elif linea.startswith("Presión"):
                     presion = linea.split(':')
                     presion_total += presion
-                    print(presion_total)
 
                 elif linea.startswith("Viento"):
                     viento = linea.split(':')
                     velocidad_total_viento += viento[0]
-                    print(velocidad_total_viento)
+            print(data)
+
 
         DatosMeteorologicos(r'C:\Users\Bas\PycharmProjects\ActividadParticipacion9\DatosMeteorologicos\datos_meteorologicos.txt')
